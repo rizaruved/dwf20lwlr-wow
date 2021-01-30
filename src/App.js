bea48e628b68de9cfe05692c14449ec4863b5fc8
Shapribrahim <shapribrahim@mails.my.id>
2021-01-30 11:52:57 +0700

    / ‘.eslintcache’ / ‘src/App.js’

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { FiHeart } from 'react-icons/fi';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Do you see what I see?
+          Do you see what I see? <FiHeart />
         </p>
       </header>
     </div>
