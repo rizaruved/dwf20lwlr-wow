d5bd656f7652297e3d0ac189bf4af87bdb275ab4
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 03:08:57 +0700

    / ‘.eslintcache’ / ‘src/index.js’ / ‘src/routes/routes.js’

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Router } from 'react-router-dom';
-import PageRoutes from "../src/routes/routes";
+import PageRoute from "../src/routes/routes";
 
 ReactDOM.render(
   <BrowserRouter>
     <Router>
-      <PageRoutes />
+      <PageRoute />
     </Router>
   </BrowserRouter>,
   document.getElementById('root')
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -25,3 +25,5 @@ const PageRoute = () => {
         </Switch>
     )
 }
+
+export default PageRoute;
