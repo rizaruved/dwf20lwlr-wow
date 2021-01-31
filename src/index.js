c71b63c37bf4b8da897a308ec33f17f33a75d9ad
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 03:29:31 +0700

    / ‘.eslintcache’ / ‘src/index.js’ < ‘src/routes/Routes.js’ > ‘src/routes/RoutesConfig.js’ - ‘src/routes/routes.js’

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import PageRoute from "../src/routes/Routes";
+import PageRoute from "./routes/RoutesConfig";
 
 ReactDOM.render(
   <BrowserRouter>
