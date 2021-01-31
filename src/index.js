035d74f3d3f3bd0fc76af00a6117178e3c14101a
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 03:27:49 +0700

    / ‘.eslintcache’ / ‘src/index.js’

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import PageRoute from "../src/routes/routes";
+import PageRoute from "../src/routes/Routes";
 
 ReactDOM.render(
   <BrowserRouter>
