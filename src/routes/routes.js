f5b27fe6ae0326167d5745cac31f061f7364c756
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 00:46:17 +0700

    / ‘src/routes/routes.js’

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+
+import LandingPage from "../pages/LandingPage";
+import AfterLogin from "../pages/AfterLogin";
+import Subscribe from "../pages/Subscribe";
+import ActiveProfile from "../pages/ActiveProfile";
+import BookDetail from "../pages/BookDetail";
+import ReadBook from "../pages/ReadBook";
+import AddBook from "../pages/AddBook";
+import TransactionList from "../pages/TransactionList";
