85523137116f3700196e33c4a0deb2ec6d28f0da
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 00:34:45 +0700

    / ‘src/pages/BookDetail.js’

---
+++ src/pages/BookDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BookDetail() {
+    return ( 
+        <div>
+            Ini adalah laman BookDetail.
+        </div>
+    )
+}
+
+export default BookDetail;
