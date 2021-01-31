6923247e82d03cbf37b239330c618273a162b3d8
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 00:39:03 +0700

    / ‘src/pages/TransactionList.js’

---
+++ src/pages/TransactionList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TransactionList() {
+    return ( 
+        <div>
+            Ini adalah laman TransactionList.
+        </div>
+    )
+}
+
+export default TransactionList;
