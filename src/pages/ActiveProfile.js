6342b55f7ea60f1064fdaf4cb7ced483813735ca
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 00:38:31 +0700

    + ‘src/pages/ActiveProfile.js’ - ‘src/pages/SubscribeActiveProfile.js’

---
+++ src/pages/ActiveProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ActiveProfile() {
+    return ( 
+        <div>
+            Ini adalah laman ActiveProfile.
+        </div>
+    )
+}
+
+export default ActiveProfile;
