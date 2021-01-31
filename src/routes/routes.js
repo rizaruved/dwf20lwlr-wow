3532bcb21216cd646aca50c98929b848888473b6
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 02:52:08 +0700

    / ‘.eslintcache’ / ‘src/routes/routes.js’

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,19 +8,19 @@ import ActiveProfilePage from "../pages/ActiveProfile";
 import BookDetailPage from "../pages/BookDetail";
 import ReadBookPage from "../pages/ReadBook";
 import AddBookPage from "../pages/AddBook";
-// import TransactionListPage from "../pages/TransactionList";
+import TransactionListPage from "../pages/TransactionList";
 
 const Route = () => {
     return (
         <Switch>
             <Route path="/" exact component={LandingPage} />
-            <Route path="/after-login/" component={AfterLoginPage} />
-            <Route path="/subscribe" component={SubscribePage} />
-            <Route path="/active-profile/" component={ActiveProfilePage} />
-            <Route path="/book-detail/"component={BookDetailPage} />
-            <Route path="/read-book/" component={ReadBookPage} />
-            <Route path="/add-book" component={AddBookPage} />
-            {/* <Route path="/transaction-list/" component={TransactionListPage} /> */}
+            <Route path="/after-login/" exact component={AfterLoginPage} />
+            <Route path="/subscribe" exact component={SubscribePage} />
+            <Route path="/active-profile/" exact component={ActiveProfilePage} />
+            <Route path="/book-detail/" exact component={BookDetailPage} />
+            <Route path="/read-book/" exact component={ReadBookPage} />
+            <Route path="/add-book" exact component={AddBookPage} />
+            <Route path="/transaction-list/" exact component={TransactionListPage} />
         </Switch>
     )
 }
