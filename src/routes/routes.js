e96648463b46600adc9e599bcb4763c55d4c46dd
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 03:06:52 +0700

    / ‘.eslintcache’ / ‘src/routes/routes.js’

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
+//Import Pages
 import LandingPage from "../pages/Landing";
 import AfterLoginPage from "../pages/AfterLogin";
 import SubscribePage from "../pages/Subscribe";
