383f2976446ab8330cf31ff582fae751f55c8bb6
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 00:31:41 +0700

    / ‘src/pages/LandingPage.js’

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function LandingPage () {
-    return <div>Ini adalah laman LandingPage.</div>
+    return ( 
+        <div>
+            Ini adalah laman LandingPage.
+        </div>
+    )
 }
 
 export default LandingPage;
