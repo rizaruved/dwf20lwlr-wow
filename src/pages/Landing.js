2fda1ee188cd7512640cdccc7f0085abc3d88238
Shapribrahim <shapribrahim@mails.my.id>
2021-02-01 05:35:32 +0700

    / ‘src/pages/Landing.js’

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,15 +1,9 @@
 import React from 'react';
-// import { Col, Container, Row, Modal } from 'react-bootstrap';
-import WOW from "../assets/images/logo.png"
 
 function Landing() {
     return ( 
-        <div className="container">
-            <div className="d-flex justify-content-start row">
-                <div className="col-md-6 col-sm-8">
-                    <img src={WOW}>
-                </div>
-            </div>
+        <div>
+            Ini adalah halaman Landing.
         </div>
     )
 }
