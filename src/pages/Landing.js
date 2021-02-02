d78077afe700d934afd3e5b19961bfc1dc975293
Shapribrahim <shapribrahim@mails.my.id>
2021-02-02 11:21:16 +0700

    / ‘.eslintcache’ / ‘src/pages/Landing.js’

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PagesStyles as Styles from '../pages/PagesStyles.css';
+import WOW from '../assets/WindowOfWorld.png';
 
 function Landing() {
     return ( 
-        <div>
-            Ini adalah halaman Landing.
+        <div className="container">
+            <div className="left-landing-element">
+                <img src={WOW} className="landing-image" />
+                <p className="landing-text">
+                    Sign-up now and subscribe to enjoy all the cool and latest books - 
+                    The best book rental service provider in Indonesia
+                </p>
+                <div className="landing-btn">
+                    <button className="signup-btn">Sign Up</button>
+                    <button className="signin-btn">Sign In</button>
+                </div>
+            </div>
         </div>
     )
 }
